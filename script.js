f05d48a216b34e6e74a078e626ceb49252f18b8b
Sasi <137400818+Sasivenkatamurthy@users.noreply.github.com>
2025-02-04 22:52:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    document.getElementById('display').value = currentInput;
+}
+
+function setOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculateResult();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function calculateResult() {
+    if (previousInput === '' || currentInput === '') return;
+    let result;
+    switch (operator) {
+        case '+':
+            result = parseFloat(previousInput) + parseFloat(currentInput);
+            break;
+        case '-':
+            result = parseFloat(previousInput) - parseFloat(currentInput);
+            break;
+        case '*':
+            result = parseFloat(previousInput) * parseFloat(currentInput);
+            break;
+        case '/':
+            result = parseFloat(previousInput) / parseFloat(currentInput);
+            break;
+        default:
+            return;
+    }
+    currentInput = result.toString();
+    operator = '';
+    previousInput = '';
+    document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    document.getElementById('display').value = '';
+}
